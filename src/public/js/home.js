d35b86c79dfbad225999a52709360cff53074c7b
ptrjs <peterjose64@gmail.com>
2024-08-16 02:22:32 +0700

    login register onboarding + logic

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  // // auto redirect to login if no token detected'
-  // if (!localStorage.getItem("token")) {
-  //   window.location.href = "./login-form.html";
-  // }
-  // function
+  // auto redirect to login if no token detected'
+  if (!localStorage.getItem("token")) {
+    window.location.href = "./auth/login.html";
+  }
+  
   const disableScroll = () => {
     $(window).on("scroll.disableScroll", function (event) {
       event.preventDefault();
