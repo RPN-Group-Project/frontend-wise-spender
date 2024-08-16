461b6f76aac1d9c6309d32b651f37401b2970cf4
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-08-16 08:52:54 +0700

    change add-expense endpoint

---
+++ src/public/js/add-expense.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // auto redirect to login if no token detected'
+  if (!localStorage.getItem("token")) {
+    window.location.href = "./auth/login.html";
+  }
+});
